268750c115eaa5fea2ab886697edc970a3c2fb85
AndreiRohau <rohau.andrei@gmail.com>
2018-08-27 22:11:28 +0300

    dll updated

--- controller/src/main/resources/dll.sql
+++ controller/src/main/resources/dll.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA IF NOT EXISTS `shop` DEFAULT CHARACTER SET utf8 ;
 USE `shop` ;
 
 CREATE TABLE IF NOT EXISTS `shop`.`orders` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user` bigint(20) NOT NULL,
   `products` varchar(500) NOT NULL,
   `address` varchar(1000) NOT NULL,
@@ -13,35 +13,36 @@ CREATE TABLE IF NOT EXISTS `shop`.`orders` (
   `status` varchar(30) DEFAULT NULL,
   `dateCreated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `table_name_id_uindex` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=108 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `orders_id_uindex` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `shop`.`products` (
-  `id` bigint(20) NOT NULL,
-  `company` varchar(60) NOT NULL,
-  `name` varchar(60) NOT NULL,
-  `type` varchar(60) NOT NULL,
-  `price` varchar(60) NOT NULL,
-  `description` varchar(2000) DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `company` varchar(90) NOT NULL,
+  `name` varchar(90) NOT NULL,
+  `type` varchar(90) NOT NULL,
+  `price` varchar(90) NOT NULL,
+  `description` varchar(5000) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `table_name_id_uindex` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `products_id_uindex` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `shop`.`reserve` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `userId` bigint(20) NOT NULL,
   `productId` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `reserve_id_uindex` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `shop`.`users` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `login` varchar(60) NOT NULL,
   `password` varchar(60) NOT NULL,
-  `role` varchar(20) NOT NULL,
+  `role` varchar(20) NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `users_id_uindex` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `users_login_uindex` (`login`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 INSERT INTO `shop`.`users` (`id`, `login`, `password`, `role`) VALUES ('1', 'admin', 'admin', 'admin');
